a9e56680af086d2960d9d64d44e1cbc72e56321f
Yannic <info@huya.ch>
2018-12-01 16:38:58 +0100

    movement Scene commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public enum Animation { Idle, Bobbing };
+
+    float originalY;
+
+    public float floatStrength = 1;
+    public float speed;
+
+
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            originalY + ((float)Mathf.Sin(Time.time * speed) * floatStrength),
+            transform.position.z);
+    }
+}
+
